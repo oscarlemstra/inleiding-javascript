81c0d9d7482065cc90ab907d1524eff0107f25ed
oscar <oscarlemstra1@gmail.com>
2020-09-11 14:59:31 +0200

    hoofdstuk 2 af

---
+++ h02/opdr 1/vulmetjs.js
@@ -0,0 +1,10 @@
+// 0_0
+document.title = "dit hoefte niet :)";
+
+document.getElementById("paginatitel").innerText = "Dit is de paginatitel";
+document.getElementById("subkop").innerText = "Dit is de subkop";
+
+document.getElementById("aap1").src = "img/dodePlanten.png";
+document.getElementById("aap1").width = 250;
+document.getElementById("aap2").src = "img/kunst.png";
+document.getElementById("aap2").width = 250;
---
+++ h02/opdr 2/stijl.js
@@ -0,0 +1,12 @@
+document.getElementById("bl1").style.backgroundColor = "blue";
+document.getElementById("bl1").style.padding = "20px";
+
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl2").style.paddingLeft = "20px";
+document.getElementById("bl2").style.marginTop = "20px";
+
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl3").style.paddingLeft = "40px";
+document.getElementById("bl3").style.marginTop = "40px";
+
+document.getElementById("bl4").style.backgroundColor = "red";
